1d85511cb88739f9b008d8a8c09f43767a8771e8
Ganoder <changyao2@163.com>
2022-01-05 11:14:43 +0800

    test11.14

--- src/main/java/com/cqu/springdemo/controller/hellocontroller.java
+++ src/main/java/com/cqu/springdemo/controller/hellocontroller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class hellocontroller {
     @RequestMapping("/hello")
     public String hello(){
-        return "hello";
+        return "hello22";
 //        lllll
     }
 
