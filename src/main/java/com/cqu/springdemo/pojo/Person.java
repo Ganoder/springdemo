9e6d14ea2960bf0a99a37b2035d71bfef1a4a391
Ganoder <changyao2@163.com>
2022-01-05 18:41:28 +0800

    learning

---
+++ src/main/java/com/cqu/springdemo/pojo/Person.java
@@ -0,0 +1,45 @@
+package com.cqu.springdemo.pojo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", dog=" + dog +
+                '}';
+    }
+
+    private String name;
+    private int age;
+    private Dog dog;
+}
--- src/test/java/com/cqu/springdemo/SpringdemoApplicationTests.java
+++ src/test/java/com/cqu/springdemo/SpringdemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.cqu.springdemo;
 
+import com.cqu.springdemo.pojo.Dog;
+import com.cqu.springdemo.pojo.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringdemoApplicationTests {
-
+ @Autowired
+    private Dog dog;
+ @Autowired
+ private Person person;
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
