2a1f8fcf452b45fef354bb27a033aadf2d6ffc3f
Ganoder <changyao2@163.com>
2022-01-06 15:22:31 +0800

    20220105

---
+++ src/main/java/com/cqu/springdemo/config/MyMVCConfig.java
@@ -0,0 +1,9 @@
+package com.cqu.springdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyMVCConfig implements WebMvcConfigurer {
+
+}
--- src/main/java/com/cqu/springdemo/controller/indexcontroller.java
+++ src/main/java/com/cqu/springdemo/controller/indexcontroller.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 @Controller
 public class indexcontroller {
     @RequestMapping("/test")
     public String test(Model model){
         model.addAttribute("name","changyao");
+        model.addAttribute("users", Arrays.asList("changyao","nishuang"));
         return "test";
     }
 }
